a72f683894dfcc222d926f9ae25ec9e56e423d1e
Vyacheslav Krestinenko <vk.lazy@gmail.com>
2023-11-16 15:18:24 +0400

    add typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import {defineConfig} from "vite";
 import {resolve} from "path";
 import handlebars from "vite-plugin-handlebars";
 
-export default defineConfig({
+export default {
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, "src/partials"),
@@ -26,4 +25,4 @@ export default defineConfig({
   server: {
     port: 3000,
   }
-});
+};
